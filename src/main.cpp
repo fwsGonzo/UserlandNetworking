938c60f316dfe5ed4d0cd623d0636eb25e4bab4c
fwsGonzo <fwsgonzo@hotmail.com>
2017-04-17 18:07:19 +0200

    Add comments and gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
-#include <cstdio>
-#include <cstdint>
-
 #include <net/inet4.hpp>
 #include "usernet.hpp"
 static char statman_data[8192];
 
 int main(void)
 {
+  // needed for stats
   Statman::init((uintptr_t) statman_data, sizeof(statman_data));
 
+  // the network driver
   UserNet    network_driver;
+  // the network stack
   net::Inet4 network((hw::Nic&) network_driver);
-
   network.network_config(
     { 10,  0,  0,  2},
     {255,255,255,  0},
     { 10,  0,  0,  1}, // GW
     {  8,  8,  8,  8}  // DNS
   );
-  printf("Hello world!\n");
 
+  printf("Hello world!\n");
 }
