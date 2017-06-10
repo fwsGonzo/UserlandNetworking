2734a60aecb62ebacfb54f732316097233586abf
Andreas Happe <andreashappe@snikt.net>
2017-06-10 15:58:58 +0200

    with this, I get afl crashes

--- src/fuzz_ip4.cpp
+++ src/fuzz_ip4.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <net/inet4>
 #include <net/ethernet/header.hpp>
 
@@ -6,6 +8,10 @@ static net::BufferStore bufstore(256, 1500);
 void fuzz_ip4(net::Inet4& network,
               const void* data, uint16_t packet_len)
 {
+
+
+	std::cerr << "packet_len: " << packet_len << std::endl;
+
   auto buffer = bufstore.get_buffer();
   // wrap in packet, pass to Link-layer
   auto* ptr = (net::Packet*) buffer.addr;
